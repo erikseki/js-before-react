96d209afabd84cdbad36e5841d87fa727ffcb651
Erik Seki <56083781+erikseki@users.noreply.github.com>
2022-09-28 00:37:12 -0300

    Desestruturação nos Objeto

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Restruturação do Js e suas principais funcionalidades importantes do JavaScript. 
 
 - Nullish Coalescing Operator
+- Objetos
+- Desestruturação 
--- main.js
+++ main.js
@@ -7,17 +7,56 @@ const user = {
   }, 
 };
 
-document.body.innerText = JSON.stringify(Object.entries(user)); 
+// Desestruturação - Remover parte do objeto para uma variável a parte, ou seja...
 
-console.log(user)
+// ao invés disso - const address = user.address, eu consigo fazer...
+const {address} = user
+
+// conseguimos também trocar o nome das keys fazendo (idade : age) na variável e imprimindo como 'age' ao invés de idade
+const {idade : age} = user
+
+// document.body.innerText = JSON.stringify(Object.entries({age})); 
 
 
-// document.body.innerText = ('name' in user); - puxando uma chave e retornando se existe ou não com true ou false 
+// conseguimos adicionar uma nova chave com ou sem valores dentro da variável, contudo a preferencia do resultado sempre será no que estiver no objeto (escopo)
+const {nickname = 'seki'} = user
 
-// document.body.innerText = Object.keys(user) - retornando todas as chaves (name, idade, address)
 
-// document.body.innerText = Object.values(user) - retornando todos os valores das chaves (erik, 22, object, object) "object porq o browser n sabe como apresentar os valores"
+/* a Desestruturação também é utilizado nas funções. 
 
-// document.body.innerText = JSON.stringify(Object.values(user)); - com o 'JSON.stringify' os valores dentro de uma objeto filho conseguem ser apresentados no browser 
 
-// document.body.innerText = JSON.stringify(Object.entries(user)); - com o 'Object.entries' conseguimos mostrar os vetores de chaves e valores organizados pelos colchetes
+function mostrarIdade(user) {
+  return user.idade;
+}
+
+ou adicionar dentro do parâmetro um '{}' para um novo objeto 
+
+
+function mostrarIdade({ idade}) {
+  return idade;
+}
+
+ou classificar dentro do parâmetro as chaves e valores quando não há nenhum
+
+
+function mostrarIdade({ idade = 24}) {
+  return idade;
+}
+
+em qualquer lugar que lidamos com objetos seja parametros de funções, variáveis, construtores e métodos de classes. 
+
+
+*/
+
+
+document.body.innerText = mostrarIdade(user)
+
+
+// document.body.innerText = JSON.stringify(Object.entries({mostrarIdade}));
+
+
+
+
+
+
+
